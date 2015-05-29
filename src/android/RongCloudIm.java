1efcb26878d9ff940066cb9671eed1366328a9f4
hzhou <zhb12345@gmail.com>
2015-05-30 01:26:11 +0800

    basically can compile android

--- src/android/RongCloudIm.java
+++ src/android/RongCloudIm.java
@@ -10,7 +10,7 @@ import org.json.JSONObject;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class CDVRongCloudIm extends CordovaPlugin {
+public class RongCloudIm extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
